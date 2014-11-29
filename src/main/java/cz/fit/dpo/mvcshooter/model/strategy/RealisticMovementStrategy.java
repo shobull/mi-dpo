2f5ff541050347baa77c96fb513c79b208215a68
Lubos Palisek <lubos.palisek@koboz.cz>
2014-11-29 11:27:49 +0100

    Pohyb po primce - moc nefunguje :X

--- src/main/java/cz/fit/dpo/mvcshooter/model/strategy/RealisticMovementStrategy.java
+++ src/main/java/cz/fit/dpo/mvcshooter/model/strategy/RealisticMovementStrategy.java
@@ -17,8 +17,13 @@ public class RealisticMovementStrategy implements IMovementStrategy {
 	@Override
 	public Coordinates move(int gravity, Missile missile) {
 		int force = (missile.getForce() / 4) == 0 ? 1 : (missile.getForce() / 4);
-		int x = missile.getFirstX() + (missile.getTime() * force);
-		int y = missile.getFirstY();
+
+		double xTemp = Math.acos(Math.toRadians(missile.getAngle())) * (missile.getTime() * force);
+		int x = (int) (missile.getFirstX() + xTemp);
+
+		double yTemp = Math.asin(Math.toRadians(missile.getAngle())) * (missile.getTime() * force);
+		int y = (int) (missile.getFirstY() + yTemp);
+
 		return new Coordinates(x, y);
 	}
 }
